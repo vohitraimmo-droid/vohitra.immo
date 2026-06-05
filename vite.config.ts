e4cd6de1e10fb62e27b8eed71d7faae4ffd585bc
vohitraimmo-droid <vohitra.immo@gmail.com>
2026-06-05 08:35:56 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,12 @@
-import { createApp } from "vinxi";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { defineConfig } from "@tanstack/react-start/config";
 import viteTsConfigPaths from "vite-tsconfig-paths";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-export default createApp({
-  routers: [
-    {
-      name: "public",
-      type: "static",
-      dir: "./public",
-    },
-    {
-      name: "client",
-      type: "client",
-      handler: "./src/entry-client.tsx",
-      target: "browser",
-      plugins: () => [
-        TanStackRouterVite({ autoCodeSplitting: true }),
-        react(),
-        tailwindcss(),
-        viteTsConfigPaths({ root: "./" }),
-      ],
-      base: "/_build",
-    },
-    {
-      name: "ssr",
-      type: "http",
-      handler: "./src/entry-server.tsx",
-      target: "server",
-      plugins: () => [
-        TanStackRouterVite({ autoCodeSplitting: true }),
-        react(),
-        tailwindcss(),
-        viteTsConfigPaths({ root: "./" }),
-      ],
-    },
-  ],
+export default defineConfig({
+  server: {
+    preset: process.env.NITRO_PRESET ?? "vercel",
+  },
+  vite: {
+    plugins: [viteTsConfigPaths({ root: "./" })],
+    build: { chunkSizeWarningLimit: 5000 },
+  },
 });
